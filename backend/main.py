a8fb6399756d4b1e4b127082e106cedb597b5ab6
Leila Adams <leila.n.adams@gmail.com>
2025-06-13 08:20:31 -0400

    whitelist domain

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",], 
+    allow_origins=["http://localhost:3000", "https://thatcatcancode.github.io/my-portfolio/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
